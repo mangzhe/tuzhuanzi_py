82f13121cedf8ad2896d085facb77de426923404
mangzhe <mangzhewuju@qq.com>
2021-12-09 04:13:28 +0800

    基本功能实现

---
+++ file/picturetochars.py
@@ -0,0 +1,59 @@
+#Author : mangzhe
+#Creat time :2021/12/09
+#Think of desige : no
+#--------------------
+from PIL import Image
+import argparse
+
+
+
+parser = argparse.ArgumentParser()
+
+parser.description="图片转字符文件"
+
+parser.add_argument("-p",help="图片路径")
+parser.add_argument("-w",help="指定宽度",type=int,default=100)
+
+args = parser.parse_args()
+
+
+PASH = args.p
+
+WIDTH = args.w
+
+chars=list("💯💢♨️🚷🚯🚳🚱🔞📵🚫⭕️🛑⛔️📛📛❌㊗️🈴🈵🈹❤️🧡💛💚💙💜🖤🤍🤎💔❣️💕💞💓💗💖📗📘📙📒")
+
+
+def x_y(r,g,b,alpha=256):
+    huidu = int(( r + g + b ) /3)
+
+    value = int(huidu * len(chars) / alpha)
+
+    return value
+
+
+
+
+def main():
+    im=Image.open(PASH)
+    HEIGHT=int(im.height * WIDTH / im.width)
+    im = im.resize(( WIDTH,HEIGHT ), Image.NEAREST)
+    TXT=""
+    for j in range(WIDTH):
+        for i in range(HEIGHT):
+            TXT += chars[ x_y( *im.getpixel((j,i)) )]
+        TXT+="\n"
+
+    with open("out.txt",'w') as files:
+        files.write(TXT)
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    main()
