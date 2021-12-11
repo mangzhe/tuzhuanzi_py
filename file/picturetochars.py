7b8bf8af1ea0661f3f6b9592fec15fb1d02729f2
mangzhe <mangzhewuju@qq.com>
2021-12-11 18:18:14 +0800

    change a little code of char[]

--- file/picturetochars.py
+++ file/picturetochars.py
@@ -1,4 +1,4 @@
-#/usr/bin/python
+#!/usr/bin/python
 #-*- encoding : utf-8 -*-
 #Author : mangzhe
 #Creat time :2021/12/09
@@ -23,7 +23,11 @@ PASH = args.p
 
 WIDTH = args.w
 
-chars=list("!1?*%&#@")
+#chars=list("!1?*%&#@")
+chars=list(".-!?*+%&#@")
+#chars=list("abcdefghjklmnopqlstuvwxy")
+#chars=list("01")
+#chars=list("0123456789")
 
 
 def x_y(r,g,b,alpha=256):
@@ -46,7 +50,7 @@ def main():
         for i in range(HEIGHT):
             TXT += chars[ x_y( *im.getpixel( (j,i) )[:3]  ) ] + " "
         TXT+="\n"
-
+#    print(TXT)
     with open("out.txt",'w') as files:
         files.write(TXT)
 
